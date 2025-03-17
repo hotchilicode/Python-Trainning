d9699cf310267178b589572bc7eea51b3668cc54
chiliwilys <siticay28@kurbieh.com>
2025-03-17 16:55:19 -0600

    circle

---
+++ Reverse_full__ame.py
@@ -0,0 +1,10 @@
+def reverse_name():
+    name = input("Enter your name: ")
+    reversed_name = name[::-1] # [start:end:step]
+    return reversed_name
+
+result = reverse_name()
+print(result)
+
+
+#This is the way you can reverse a string
