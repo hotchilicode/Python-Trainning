c3bc10161fcfb7e0b971865a58cf589a0599ba47
chiliwilys <siticay28@kurbieh.com>
2025-03-16 19:20:31 -0600

    formatted poem

---
+++ twinkle_formatted.py
@@ -0,0 +1,4 @@
+print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow i wonder what you are')
+
+#\n is for new line
+#\t is for tab
